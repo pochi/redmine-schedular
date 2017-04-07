3f55b83237daa3e1bc3a44216add195a2f809af8
Shota Suzuki <susi29gari@gmail.com>
2017-04-07 14:35:32 +0900

    fix routes(match->http method)

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 
 Rails.application.routes.draw do
   resources :projects do
-    match '/schedulers/home' => 'schedulers#home'
+    get '/schedulers/home' => 'schedulers#home'
     resources :schedulers do
       resources :events, :only => [:create, :update, :destroy]
       resource :participations, :only => [:destroy]
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     end
   end
 
-  match '/schedulers/settings', :to => 'schedulers#settings', :via => :post
+  post '/schedulers/settings', :to => 'schedulers#settings'
   resources :scheduler_custom_list_contents
   resources :scheduler_settings
 end
